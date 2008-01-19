dd813f877db6c0f0bde0ffa9f131ca83c1d5b985
Andrew Cowie <andrew@operationaldynamics.com>
2008-01-19 14:09:55 +1100

    Add a preliminary .desktop file
    I don't claim to have any idea about how these files are actually used
    by GNOME, but this one seems like a step in the right direction.
    Unfortunately, it contains more hard coded nonsense; I don't really
    want to resort to m4 so we'll have to see how best to approach this.

--- src/C/launcher/launcher.c
+++ src/C/launcher/launcher.c
@@ -14,8 +14,8 @@ int main(int argc, char** argv) {
 		execlp("/opt/sun-jdk-1.5.0.13/bin/java",
 			"/opt/sun-jdk-1.5.0.13/bin/java", 	
 			"-client", 
-			"-classpath","/home/andrew/src/andrew/java-gnome/treeview/tmp/gtk-4.0.jar:tmp/classes",
-        		"-Djava.library.path=/home/andrew/src/andrew/java-gnome/treeview/tmp:tmp",
+			"-classpath","/home/andrew/src/andrew/java-gnome/primary/tmp/gtk-4.0.jar:tmp/classes",
+        		"-Djava.library.path=/home/andrew/src/andrew/java-gnome/primary/tmp:tmp",
         		"com.operationaldynamics.slashtime.Master",
         		NULL);
         	return errno;
