2a76e0d23b3a10deb10e89167a253d067998b445
Andrew Cowie <andrew@operationaldynamics.com>
2008-08-18 10:13:36 +1000

    Bump development version

--- src/java/slashtime/client/Version.java
+++ src/java/slashtime/client/Version.java
@@ -14,5 +14,5 @@ public interface Version
 {
     public static final String APIVERSION = "0.6";
 
-    public static final String VERSION = "0.5.6";
+    public static final String VERSION = "0.5.7";
 }
