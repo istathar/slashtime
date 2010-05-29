baa02de6c1f4483bf5a0a5ca7bbbb61438720d23
Andrew Cowie <andrew@operationaldynamics.com>
2010-05-29 11:07:53 +1000

    Move translatable GPL licence header
    Move the full GPL licence header from being a translatable String to
    being "licence-text". Place canonical version of this in po/en.po.
    
    [Should there be a po/C.po for this?]

--- src/java/slashtime/ui/UserInterface.java
+++ src/java/slashtime/ui/UserInterface.java
@@ -93,18 +93,7 @@ public class UserInterface
         dialog.setComments(_("Show the time in various places!"));
         dialog.setCopyright(_("Copyright")
                 + " \u00A9 2003-2010 Operational Dynamics Consulting Pty Ltd, " + _("and Others"));
-        dialog.setLicense(_("The code in this file, and the program it is a part of, is made available "
-                + "to you by its authors as open source software: you can redistribute it "
-                + "and/or modify it under the terms of the GNU General Public License version "
-                + "2 (\"GPL\") as published by the Free Software Foundation."
-                + "\n\n"
-                + "This program is distributed in the hope that it will be useful, but WITHOUT "
-                + "ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or "
-                + "FITNESS FOR A PARTICULAR PURPOSE. See the GPL for more details."
-                + "\n\n"
-                + "You should have received a copy of the GPL along with this program. If not, "
-                + "see http://\u200Dwww.gnu.org/\u200Dlicenses/. The authors of this program may be "
-                + "contacted through http://\u200Dresearch.operationaldynamics.com/\u200Dprojects/\u200Dslashtime/"));
+        dialog.setLicense(_("licence-text"));
         dialog.setLogo(images.marble);
         dialog.setTranslatorCredits(_("translator-credits"));
         dialog.setVersion(Version.VERSION);
