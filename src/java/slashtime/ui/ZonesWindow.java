5eeadcceeed1c8622dd4458f02631bee5ff1c66a
Yaakov Selkowitz <yselkowi@redhat.com>
2021-12-23 22:57:24 -0500

    Update all URLs

--- src/java/slashtime/ui/ZonesWindow.java
+++ src/java/slashtime/ui/ZonesWindow.java
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GPL along with this program. If not,
  * see http://www.gnu.org/licenses/. The authors of this program may be
- * contacted through http://research.operationaldynamics.com/projects/slashtime/.
+ * contacted through https://github.com/istathar/slashtime/.
  */
 package slashtime.ui;
 
