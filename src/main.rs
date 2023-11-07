22ced455b43f485eddbfc2373e744db8953cb490
Andrew Cowie <istathar@gmail.com>
2023-11-08 10:02:24 +1100

    Preliminary TimeZone name parsing experiment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use chrono::{DateTime, Datelike, Local, NaiveDate, TimeZone, Timelike, Utc};
+use chrono_tz::Tz;
 
 fn main() {
     let local_now = Local::now();
-    println!("Local now: {}", local_now);
+    println!("Local now: {}", local_now.to_rfc3339());
+
+    let tz: Tz = "US/Eastern".parse().unwrap();
+
+    let dt = tz.with_ymd_and_hms(1969, 07, 20, 16, 17, 00).unwrap();
+    let dt2 = dt.with_timezone(&Utc);
+    println!("The Eagle has landed: {}", dt2.to_rfc3339());
+
+    // Get the current time zone as a string.
+    let tz_str = iana_time_zone::get_timezone().unwrap();
+    println!("The current time zone is: {}", tz_str);
 }
