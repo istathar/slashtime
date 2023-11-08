46a67020c39534be4a784fe58a0350620bc940e6
Andrew Cowie <istathar@gmail.com>
2023-11-08 13:46:52 +1100

    One more experiment with chrono-tz

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,15 @@ fn main() {
     let tz: Tz = "US/Eastern".parse().unwrap();
 
     let dt = tz.with_ymd_and_hms(1969, 07, 20, 16, 17, 00).unwrap();
+    println!("The Eagle has landed: {}", dt.to_rfc3339());
+
     let dt2 = dt.with_timezone(&Utc);
     println!("The Eagle has landed: {}", dt2.to_rfc3339());
 
     // Get the current time zone as a string.
     let tz_str = iana_time_zone::get_timezone().unwrap();
     println!("The current time zone is: {}", tz_str);
+
+    let dt3 = dt.with_timezone(&Local);
+    println!("The Eagle has landed: {}", dt3.to_rfc3339());
 }
