b4c63b11746a56fddf1a11987593f5d9f541364b
Andrew Cowie <istathar@gmail.com>
2023-11-20 13:53:37 +1100

    Experiments with tz-rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
-use chrono::{DateTime, Datelike, Local, NaiveDate, TimeZone, Timelike, Utc};
-use chrono_tz::Tz;
+use tz::TimeZone;
+use tz::TimeZoneRef;
+use tz::TzError;
 
-fn main() {
-    let local_now = Local::now();
-    println!("Local now: {}", local_now.to_rfc3339());
+fn main() -> Result<(), TzError> {
+    let lima = TimeZone::local()?;
+    let xray = tz::TimeZone::from_posix_tz("Australia/Sydney")?;
+    let utc = TimeZoneRef::utc();
+    let now = tz::UtcDateTime::now()?;
+    println!("UTC now:\t{}", now);
 
-    let tz: Tz = "US/Eastern".parse().unwrap();
+    let now2 = tz::DateTime::now(lima.as_ref())?;
+    println!("Local now:\t{}", now2);
 
-    let dt = tz.with_ymd_and_hms(1969, 07, 20, 16, 17, 00).unwrap();
-    println!("The Eagle has landed: {}", dt.to_rfc3339());
+    let east: TimeZone = tz::TimeZone::from_posix_tz("US/Eastern")?;
 
-    let dt2 = dt.with_timezone(&Utc);
-    println!("The Eagle has landed: {}", dt2.to_rfc3339());
+    let now3 = tz::DateTime::now(east.as_ref())?;
+    println!("Eastern time:\t{}", now3);
+
+    // now we make a historical date
+
+    let tranquility = tz::DateTime::find(1969, 07, 20, 16, 17, 00, 0, east.as_ref())?
+        .earliest()
+        .unwrap();
+
+    println!("The Eagle has landed:\n\t\t{}", tranquility);
+
+    let tranquility2 = tranquility.project(utc)?;
+    println!("The Eagle has landed:\n\t\t{}", tranquility2);
+
+    let tranquility3 = tranquility.project(xray.as_ref())?;
+    println!("The Eagle has landed:\n\t\t{}", tranquility3);
 
     // Get the current time zone as a string.
     let tz_str = iana_time_zone::get_timezone().unwrap();
     println!("The current time zone is: {}", tz_str);
 
-    let dt3 = dt.with_timezone(&Local);
-    println!("The Eagle has landed: {}", dt3.to_rfc3339());
+    println!("And now {:?}", lima.find_current_local_time_type()?);
+
+    Ok(())
 }
